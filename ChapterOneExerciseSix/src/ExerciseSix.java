a708ae9b0c5e11e746c7ae0798ae54784861ef6f
ethomascappello <ethomascappello@Marist-HNSQXQ2.necsd.net>
2019-06-17 18:23:05 -0400

    Exercise Six

---
+++ ChapterOneExerciseSix/src/ExerciseSix.java
@@ -0,0 +1,10 @@
+
+public class ExerciseSix {
+
+	public static void main(String[] args) {
+		 System.out.print("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) = ");
+	        System.out.println((1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9));
+
+	}
+
+}
